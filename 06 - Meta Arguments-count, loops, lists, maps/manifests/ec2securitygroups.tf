fec3f5010882c5df9e1612c5101e76ab46f213f4
ejohnpaul <johnpaulcemenogu@gmail.com>
2023-07-16 18:59:24 -0400

    Dynamick blockss & meta arguments updated

--- 06 - Meta Arguments-count, loops, lists, maps/manifests/ec2securitygroups.tf
+++ 06 - Meta Arguments-count, loops, lists, maps/manifests/ec2securitygroups.tf
@@ -1,5 +1,5 @@
 # Create Security Group - SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
+/*resource "aws_security_group" "vpc-ssh" {
   name        = "vpc-ssh"
   description = "Dev VPC SSH"
   ingress {
@@ -11,7 +11,7 @@ resource "aws_security_group" "vpc-ssh" {
   }
 
   egress {
-    description = "Allow all ip and ports outbound"    
+    description = "Allow all ip and ports outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -40,9 +40,9 @@ resource "aws_security_group" "vpc-web" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
   egress {
-    description = "Allow all ip and ports outbound"    
+    description = "Allow all ip and ports outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -52,5 +52,36 @@ resource "aws_security_group" "vpc-web" {
   tags = {
     Name = "vpc-web"
   }
+}*/
+
+resource "aws_security_group" "vpc-ssh" {
+  name = "Allow HTTPS"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "vpc-ssh"
+  }
 }
+
 
